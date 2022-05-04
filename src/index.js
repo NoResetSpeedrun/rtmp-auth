3b4b3ee1063d9819b7e73c5486072fcc56b15732
Yannick Houle <shokushuu@gmail.com>
2022-05-04 12:12:50 -0400

    changed values for discord auth redirection

--- src/index.js
+++ src/index.js
@@ -15,7 +15,21 @@ app.get('/discord-auth', async (req, res) => {
   try {
     const user = await storeUser(req);
     const queryParams = new URLSearchParams({ stream_key: `${user.username}?token=${user.token}` });
-    res.redirect(`http://www.noresetspeed.run/en/profile/discord?${queryParams.toString()}`);
+    const hostname = req.headers['x-forwarded-host'];
+    const response = {
+      'localhost' : 'http://localhost:3000/api/v1/user/discord/link', 
+      'dev.noreset.tv' : 'https://dev.noreset.tv/api/v1/user/discord/link',
+      'noreset.tv' : 'https://noreset.tv/api/v1/user/discord/link',
+      'noresetspeed.run' : 'http://www.noresetspeed.run/en/profile/discord',
+    }
+    console.log({hostname});
+    console.log('Response hostname : ', response[hostname]);
+    if(response[hostname]){
+      res.redirect(`${response[hostname]}?${queryParams.toString()}`);
+    }else{
+      res.redirect(`https://error.noreset.tv/`);
+    }
+    
   } catch (e) {
     console.log(e);
     res.status(500).send('Invalid request');
