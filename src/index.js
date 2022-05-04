6cc15a13909f6574e09522b15fc047f1ffff98c6
Yannick Houle <shokushuu@gmail.com>
2022-05-04 12:58:32 -0400

    reverted back to single redirect

--- src/index.js
+++ src/index.js
@@ -15,22 +15,16 @@ app.get('/discord-auth', async (req, res) => {
   try {
     const user = await storeUser(req);
     const queryParams = new URLSearchParams({ stream_key: `${user.username}?token=${user.token}` });
-    const currentReq = await req;
-    const hostname = await req.headers['x-forwarded-host'];
-    const response = {
+    
+    /*const response = {
       'localhost' : 'http://localhost:3000/api/v1/user/discord/link', 
       'dev.noreset.tv' : 'https://dev.noreset.tv/api/v1/user/discord/link',
       'noreset.tv' : 'https://noreset.tv/api/v1/user/discord/link',
       'noresetspeed.run' : 'http://www.noresetspeed.run/en/profile/discord',
-    }
-    console.log({currentReq});
-    console.log({hostname});
-    console.log('Response hostname : ', response[hostname]);
-    if(response[hostname]){
-      res.redirect(`${response[hostname]}?${queryParams.toString()}`);
-    }else{
-      res.redirect(`https://error.noreset.tv/`);
-    }
+    }*/
+    
+    res.redirect(`https://dev.noreset.tv/api/v1/user/discord/link?${queryParams.toString()}`);
+    
     
   } catch (e) {
     console.log(e);
